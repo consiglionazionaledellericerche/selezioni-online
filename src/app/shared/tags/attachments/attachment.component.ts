ca5222262f594fe6cb6bf36bd23551f4ea699e04
SPASIANO MARCO <marco.spasiano@cnr.it>
2021-04-14 20:18:02 +0000

    Update attachment.component.ts

--- src/app/shared/tags/attachments/attachment.component.ts
+++ src/app/shared/tags/attachments/attachment.component.ts
@@ -22,11 +22,11 @@ import {ValidationHelper} from '../../../common/validation/validation-helper';
               [ttipAppend]="ttipAppend"
               [noLabel]="noLabel"
               [showValidation]="showValidation">
-          <button
+          <label
             #label 
             for="upload"
-            class="btn btn-light btn-block border-bottom mb-0" 
-            [ngClass]="{'is-valid border-dark text-dark': !isInvalid(), 'is-invalid border-danger text-danger': isInvalid()}">{{ 'upload' | translate }}</button>
+            class="border-bottom w-100 mb-0" 
+            [ngClass]="{'is-valid border-dark text-dark': !isInvalid(), 'is-invalid border-danger text-danger': isInvalid()}">{{ 'upload' | translate }}</label>
           <input class="form-control d-none"
              type="file"
              id="upload"
